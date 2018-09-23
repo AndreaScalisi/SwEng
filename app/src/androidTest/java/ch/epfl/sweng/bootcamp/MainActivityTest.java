b0747f25893cbfd168f340664c008d344ecab19c
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-09-23 12:31:12 +0200

    LAST CODECLIMATE ISSUE Modified one more import

--- app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -1,7 +1,7 @@
 package ch.epfl.sweng.bootcamp;
 
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
